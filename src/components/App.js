3dc645f0812262a1a5b495e7f9e6e98107fa65ed
Scott Spence <scottspence10@gmail.com>
2017-03-02 22:26:04 +0000

    add app component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router'
+
+import App from './components/App'
+
+class App extends React.Component {
+    constructor () {
+        
+    }
+    
+  render () {
+    return (
+      <div className='fast-diet-picker'>
+      </div>
+    )
+  }
+}    
+
+export default App
