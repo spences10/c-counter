e8ed73a3ff79e8402fa61d920373e88b00f13848
spences10 <spences10apps@gmail.com>
2017-03-13 06:53:33 +0000

    Haz app! :+1:

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter, Match, Miss } from 'react-router'
-
-import App from './components/App'
 
 class App extends React.Component {
-    constructor () {
-        
-    }
-    
   render () {
     return (
-      <div className='fast-diet-picker'>
-      </div>
+      <p>Hello</p>
     )
   }
-}    
+}
+
 
 export default App
