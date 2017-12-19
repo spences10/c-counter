c27365fb51231d7cbee0e7f5034fa057ac0935d5
spences10 <spences10apps@gmail.com>
2017-12-19 09:08:47 +0000

    add injectGlobal, ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider, injectGlobal } from 'styled-components'
 
 import registerServiceWorker from './registerServiceWorker'
 
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 2rem;
+    font-family: -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Helvetica,
+      Arial,
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+`
+
+const theme = {
+  primary: '#a04ed9'
+}
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker()
