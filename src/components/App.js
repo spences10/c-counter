40d1d1afb456bc5b733e10e16cb84a1047a83d91
spences10 <spences10apps@gmail.com>
2017-12-19 09:11:11 +0000

    Add some components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
 
-class App extends Component {
-  render() {
-    return <div className="App">yo!</div>
-  }
-}
+import Button from './Button'
+import Quote from './Quote'
+import Notice from './Notice'
+
+const App = () => (
+  <div>
+    <Button>Hey, click me!</Button>
+    <Notice>
+      Just letting you know... We think you're awesome and we're super happy to
+      have you as a customer!
+    </Notice>
+    <Quote>Wooooaa, this is sooo so cool my friends!!</Quote>
+  </div>
+)
 
 export default App
