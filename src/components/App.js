e58def1772925d264d0945fd58cfe6dcd7dba77d
spences10 <spences10apps@gmail.com>
2018-02-12 13:14:35 +0000

    move state to app

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,42 @@ import Notice from './Notice'
 import Tickers from './Tickers'
 // import Cryptocurrency from './Cryptocurrency'
 
-const App = () => (
-  <div>
-    <Quote>Cryptocurrency tickers</Quote>
-    <Tickers />
-    <Notice>
-      Information updated every 10 seconds courtesy of
-      coinmarketcap.com
-    </Notice>
-    {/* <Button>Hey, click me!</Button> */}
-  </div>
-)
+import { fetchCryptocurrencyData } from '../util/helpers'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    fetchCryptocurrencyData(
+      'https://api.coinmarketcap.com/v1/ticker/?convert=GBP&limit=50'
+    ).then(result => {
+      this.setState({ data: result.data })
+    })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.data.map(items => {
+          console.log('====================')
+          console.log(items)
+          console.log('====================')
+          return <p key={items.id}>{items.id}</p>
+        })}
+        {/* <Quote>Cryptocurrency tickers</Quote>
+        <Tickers />
+        <Notice>
+          Information updated every 10 seconds courtesy of
+          coinmarketcap.com
+        </Notice> */}
+        {/* <Button>Hey, click me!</Button> */}
+      </div>
+    )
+  }
+}
 
 export default App
