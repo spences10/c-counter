af6e82e184a05056030f707701be4b6ca197eedc
spences10 <spences10apps@gmail.com>
2018-02-12 22:33:56 +0000

    remove unused exports

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,4 +1,4 @@
-import styled, { injectGlobal } from 'styled-components'
+import { injectGlobal } from 'styled-components'
 
 export const theme = {
   black: '#272727',
@@ -26,7 +26,7 @@ export const theme = {
 }
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto|VT323');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto');
 
   body {
     margin: 0;
@@ -36,55 +36,4 @@ injectGlobal`
       Arial,
       sans-serif;
   }
-`
-
-export const StyledH1 = styled.h1`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledH2 = styled.h2`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledH3 = styled.h3`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledH4 = styled.h4`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledH5 = styled.h5`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledH6 = styled.h6`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledP = styled.p`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledUl = styled.ul`
-  padding: 1.75rem;
-  margin: 0.5rem;
-`
-
-export const StyledLi = styled.li`
-  padding: 1.75rem;
-  margin: 0.5rem;
-  text-decoration: none;
-`
-
-export const StyledA = styled.a`
-  padding: 1.75rem;
-  margin: 0.5rem;
 `
