b714bc7a53d37ff261e8c4bd475ff6d23a4745ae
spences10 <spences10apps@gmail.com>
2018-02-12 22:34:51 +0000

    name and symbol on one line

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas: 'n n s';
+
   border: 1px solid ${props => props.theme.black};
   border-radius: 4px;
 `
 
+const Name = styled.h1`
+  margin: 1rem;
+  padding: 1rem;
+  grid-area: n;
+`
+
+const Symbol = styled.p`
+  margin: 1rem;
+  padding: 1rem;
+  grid-area: s;
+  align-self: center;
+`
+
+const PctChange = styled.p``
+
 const Cryptocurrency = props => {
   const {
-    id,
     name,
     symbol,
     price_usd,
@@ -20,17 +39,17 @@ const Cryptocurrency = props => {
   } = props
 
   console.log('====================')
-  console.log(props)
+  console.log('PROPS ', props)
   console.log('====================')
 
   return (
     <CardWrapper>
-      <p>{id}</p>
-      <p>
-        {name} {symbol}
-      </p>
-      <p>{parseFloat(price_usd).toFixed(2)}</p>
-      <p>{parseFloat(price_gbp).toFixed(2)}</p>
+      <Name>{name}</Name> <Symbol>({symbol})</Symbol>
+      {/* <p>USD {parseFloat(price_usd).toFixed(2)}</p> */}
+      {/* <p>GBP {parseFloat(price_gbp).toFixed(2)}</p> */}
+      {/* <PctChange area={'o'}>{percent_change_1h}% 1hr</PctChange> */}
+      {/* <PctChange area={'t'}>{percent_change_24h}% 24hr</PctChange> */}
+      {/* <PctChange area={'s'}>{percent_change_7d}% 7days</PctChange> */}
     </CardWrapper>
   )
 }
