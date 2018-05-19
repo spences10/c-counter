274cbcd8fabde0aa4aa9502ee9aa84e925848153
spences10 <spences10apps@gmail.com>
2018-05-19 22:40:09 +0100

    add functions

--- contexts/CryptoDataContext.js
+++ contexts/CryptoDataContext.js
@@ -11,19 +11,68 @@ export class CryptoDataProvider extends React.Component {
     currency: 'GBP', // default to GBP
     limit: 3,
     apiUrl: 'https://api.coinmarketcap.com/v1/ticker/',
-    timeNow: formatTime(new Date()),
+    // timeNow: formatTime(new Date()),
     timeNext: '',
     currentSearch: ''
   }
 
-  componentWillMount() {
-    fetchCryptocurrencyData = () => {
+  handleCurrencyChange(e) {
+    const currency = e.target.value
+    const url = this.apiUrl(currency, this.state.limit)
+    fetchCryptocurrencyData(url).then(result => {
       this.setState({
-        item1: 2,
-        item2: 3
+        apiData: result.data,
+        results: result.data,
+        currency
       })
-    }
+    })
   }
+
+  handleLimitChange(e) {
+    const limit = e.target.value
+    const url = this.apiUrl(this.state.currency, limit)
+    fetchCryptocurrencyData(url).then(result => {
+      // set limit with result data
+      this.setState({
+        apiData: result.data,
+        results: result.data,
+        limit
+      })
+    })
+  }
+
+  handleSearch(e) {
+    const regex = new RegExp(e.target.value, 'gi')
+    this.setState({
+      results: this.state.apiData.filter(
+        item => item.name.match(regex) || item.symbol.match(regex)
+      ),
+      currentSearch: e.target.value
+    })
+  }
+
+  filterData() {
+    this.setState({
+      apiData: this.state.data.filter(item =>
+        item.name.match(this.state.currentSearch)
+      )
+    })
+  }
+
+  apiUrl() {
+    if (arguments.length === 0 || !arguments[0]) {
+      return `${this.state.apiUrl}?convert=GBP&limit=3`
+    }
+
+    if (!arguments[1]) {
+      return `${this.state.apiUrl}?convert=${arguments[0]}&limit=3`
+    }
+
+    return `${this.state.apiUrl}?convert=${arguments[0]}&limit=${
+      arguments[1]
+    }`
+  }
+
   render() {
     return (
       <CryptoDataContext.Provider
