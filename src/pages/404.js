4b70afe73932ae946db2ee8ac279b4940dcf0a54
spences10 <spences10apps@gmail.com>
2019-11-15 21:04:19 +0000

    add seo component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,23 @@
-import React from "react"
+import React from 'react'
+import SEO from 'react-seo-component'
+import Layout from '../components/layout'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export default NotFoundPage
+export default () => {
+  const { description, author } = useSiteMetadata()
+  return (
+    <Layout>
+      <SEO
+        title="404: Not found"
+        description={description || 'nothin’'}
+        // image={image}
+        // pathname={siteUrl}
+        // siteLanguage={siteLanguage}
+        // siteLocale={siteLocale}
+        twitterUsername={author}
+      />
+      <h1>NOT FOUND</h1>
+      <p>`You just hit a route that doesn't exist... the sadness.`</p>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,28 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import SEO from 'react-seo-component'
 import Layout from '../components/layout'
-import SEO from '../components/seo'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
-export default () => (
-  <Layout>
-    <SEO title="Home" />
-    <h3>
-      <Link to="/build-time-data/">Build time Data Example</Link>
-    </h3>
-    <h3>
-      <Link to="/run-time-data/">Run time Data Example</Link>
-    </h3>
-  </Layout>
-)
+export default () => {
+  const { title, description, author } = useSiteMetadata()
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        description={description || `nothin’`}
+        // image={image}
+        // pathname={siteUrl}
+        // siteLanguage={siteLanguage}
+        // siteLocale={siteLocale}
+        twitterUsername={author}
+      />
+      <h3>
+        <Link to="/build-time-data/">Build time Data Example</Link>
+      </h3>
+      <h3>
+        <Link to="/run-time-data/">Run time Data Example</Link>
+      </h3>
+    </Layout>
+  )
+}
