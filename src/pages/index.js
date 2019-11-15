43beb0a3c8292c9b2a6094f598e5b4128a57f0e8
spences10 <spences10apps@gmail.com>
2019-11-15 20:49:04 +0000

    remove unused components /assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,16 @@
 import { Link } from 'gatsby'
 import React from 'react'
-import Image from '../components/image'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 export default () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi API Days London!</h1>
-    <p>Default Gatsby starter with data fetching examples.</p>
     <h3>
       <Link to="/build-time-data/">Build time Data Example</Link>
     </h3>
     <h3>
       <Link to="/run-time-data/">Run time Data Example</Link>
     </h3>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <p>Now go build something great.</p>
   </Layout>
 )
