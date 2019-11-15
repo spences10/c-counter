560e18cf64b615fdcbb72d136295081dd4a04bc4
spences10 <spences10apps@gmail.com>
2019-11-15 14:59:42 +0000

    add REST data fetch

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,25 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const axios = require('axios')
+const createNodeHelpers = require('gatsby-node-helpers').default
 
-// You can delete this file if you're not using it
+const { createNodeFactory } = createNodeHelpers({
+  typePrefix: 'coinlore',
+})
+
+const COINLORE = createNodeFactory('COINLORE')
+
+exports.sourceNodes = async ({ actions }) => {
+  const { createNode } = actions
+
+  const fetchCoinloreData = () =>
+    axios.get('https://api.coinlore.com/api/tickers/')
+  // await for results
+  const res = await fetchCoinloreData()
+
+  // Create your node object
+  const coinloreNode = COINLORE(res.data)
+
+  // Create node with the gatsby createNode() API
+  createNode(coinloreNode)
+
+  return
+}
