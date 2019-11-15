ac35b45a3f53f7e6c6410d050ed70e0890371468
spences10 <spences10apps@gmail.com>
2019-11-15 16:35:21 +0000

    add build time query

--- src/pages/build-time-data.js
+++ src/pages/build-time-data.js
@@ -3,33 +3,46 @@ import React from 'react'
 import Layout from '../components/layout'
 
 export default ({ data }) => {
-  const {
-    rickAndMorty: { characters },
-  } = data
+  const { info, data: coinData } = data.coinloreCoinlore
   return (
     <Layout>
-      {characters.results.map(character => (
-        <div key={character.id}>
-          <h2>{character.name}</h2>
-          <p>Species: {character.species}</p>
-          <img src={character.image} />
+      <p>Data as at: {new Date(info.time * 1000).toISOString()}</p>
+      <p>Total Coins: {info.coins_num}</p>
+      {coinData.map(coin => (
+        <div style={{ border: '1px solid black', marginTop: '2px' }}>
+          <p>Symbol: {coin.symbol}</p>
+          <p>Name: {coin.name}</p>
+          <p>Price USD: {coin.price_usd}</p>
         </div>
       ))}
-      <Dump GraphQLResponse={data} />
+      <Dump info={info} GraphQLResponse={coinData} />
     </Layout>
   )
 }
 
 export const GatsbyQuery = graphql`
-  {
-    rickAndMorty {
-      characters {
-        results {
-          id
-          name
-          species
-          image
-        }
+  query CoinloreQuery {
+    coinloreCoinlore {
+      info {
+        coins_num
+        time
+      }
+      data {
+        symbol
+        name
+        nameid
+        rank
+        price_usd
+        percent_change_24h
+        percent_change_1h
+        percent_change_7d
+        price_btc
+        market_cap_usd
+        volume24
+        volume24a
+        csupply
+        tsupply
+        msupply
       }
     }
   }
