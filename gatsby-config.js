a114fe979804fe28fe0003d27de22eb380cb5248
spences10 <spences10apps@gmail.com>
2019-11-16 07:46:04 +0000

    unused image packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,15 +10,6 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
