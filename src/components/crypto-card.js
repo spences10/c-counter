244bd32011ebcf28a0c578b34d095cc135f169b3
spences10 <spences10apps@gmail.com>
2019-11-16 18:10:48 +0000

    add buil ddata and card to index

--- src/components/crypto-card.js
+++ src/components/crypto-card.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CardWrapper = styled.div`
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+`
 
 export const CryptoCard = ({ id, symbol, name, price }) => {
   return (
-    <div key={id} style={{ border: '1px solid black' }}>
+    <CardWrapper key={id}>
       <h2>{symbol}</h2>
       <h3>{name}</h3>
       <h4>{price}</h4>
-    </div>
+    </CardWrapper>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import SEO from 'react-seo-component'
+import styled from 'styled-components'
+import { CryptoCard } from '../components/crypto-card'
 import { Layout } from '../components/layout'
 import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
-export default () => {
+export const autoGrid = (minColumnWidth = 250, gridGap = 0) => ({
+  display: 'grid',
+  gridTemplateColumns: `repeat(auto-fill, minmax(${minColumnWidth}px,1fr))`,
+  gridGap,
+})
+
+const Gallery = styled.div({
+  ...autoGrid(300, 5),
+})
+
+export default ({ data }) => {
   const { title, description, author } = useSiteMetadata()
+  const { data: coinData } = data.coinloreCoinlore
+
   return (
     <Layout>
       <SEO
@@ -16,6 +30,40 @@ export default () => {
         // siteLocale={siteLocale}
         twitterUsername={author}
       />
+      <Gallery>
+        {coinData.map(coin => (
+          <CryptoCard
+            id={coin.id}
+            symbol={coin.symbol}
+            name={coin.name}
+            price={coin.price_usd}
+          />
+        ))}
+      </Gallery>
     </Layout>
   )
 }
+
+export const GatsbyQuery = graphql`
+  query CoinloreQuery {
+    coinloreCoinlore {
+      data {
+        symbol
+        name
+        nameid
+        rank
+        price_usd
+        percent_change_24h
+        percent_change_1h
+        percent_change_7d
+        price_btc
+        market_cap_usd
+        volume24
+        volume24a
+        csupply
+        tsupply
+        msupply
+      }
+    }
+  }
+`
