3fcb31902c91900b51d8f0dbeb6ac6e8a48187d7
spences10 <spences10apps@gmail.com>
2019-11-30 17:18:10 +0000

    style card

--- src/components/crypto-card.js
+++ src/components/crypto-card.js
@@ -8,14 +8,17 @@ const CardWrapper = styled.div`
   grid-template-areas:
     'name  name  name  name '
     'price price price price'
-    '. . . .';
-
+    'pct   pct   pct   pct  '
+    'p1hr  p1hr  p1hr  p1hr'
+    'p24hr p24hr p24hr p24hr'
+    'p7d   p7d   p7d   p7d';
   border-radius: 10px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
   &:hover {
-    transform: translateY(-3px);
+    /* transform: translateY(-3px); */
     transition: all 0.3s;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.8);
+    /* box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.8); */
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
   }
 `
 
@@ -52,13 +55,36 @@ const Price = styled.span`
   }
 `
 
+const PercentageChangeTitle = styled.h4`
+  grid-area: pct;
+  padding: 0.5rem;
+  margin: 0;
+  opacity: 0.8;
+`
+
+const PercentValue = styled.span`
+  position: absolute;
+  font-weight: 900;
+  right: 4rem;
+`
+
+const PercentageTitle = styled.span`
+  padding-left: 4rem;
+`
+
+const PercentageChange = styled.div`
+  grid-area: ${props => props.area};
+  padding: 0.5rem;
+  position: relative;
+`
+
 export const CryptoCard = ({
   id,
   symbol,
   name,
   price,
-  pcChange24h,
   pcChange1h,
+  pcChange24h,
   pcChange7d,
 }) => {
   return (
@@ -72,14 +98,19 @@ export const CryptoCard = ({
         {price.slice(0, price.indexOf('.'))}
         <span>{price.slice(price.indexOf('.'))}</span>
       </Price>
-      <div style={{ padding: '0.5rem' }}>
-        <p>Percent Change 1hr</p>
-        <p>{pcChange1h}</p>
-        <p>Percent Change 24hr</p>
-        <p>{pcChange24h}</p>
-        <p>Percent Change 7d</p>
-        <p>{pcChange7d}</p>
-      </div>
+      <PercentageChangeTitle>Percent Change:</PercentageChangeTitle>
+      <PercentageChange area={'p1hr'}>
+        <PercentageTitle>1hr</PercentageTitle>
+        <PercentValue>{pcChange1h}</PercentValue>
+      </PercentageChange>
+      <PercentageChange area={'p24hr'}>
+        <PercentageTitle>24hr</PercentageTitle>
+        <PercentValue>{pcChange24h}</PercentValue>
+      </PercentageChange>
+      <PercentageChange area={'p7d'}>
+        <PercentageTitle>7d</PercentageTitle>
+        <PercentValue>{pcChange7d}</PercentValue>
+      </PercentageChange>
     </CardWrapper>
   )
 }
