0aaf3a4a4d8888fcfd096e89a602ebac9606edda
spences10 <spences10apps@gmail.com>
2022-02-10 18:41:59 +0000

    feat: :sparkles: fetch coinlore data add to component

---
+++ src/lib/components/crypto-card.svelte
@@ -0,0 +1,73 @@
+<script>
+  export let currency
+
+  let {
+    symbol,
+    name,
+    price_usd,
+    percent_change_1h,
+    percent_change_24h,
+    percent_change_7d,
+  } = currency
+</script>
+
+<section class="card-wrapper">
+  <div class="title">
+    <p>{name}</p>
+    <p>{symbol}</p>
+  </div>
+  <p class="price">${price_usd}</p>
+  <div class="percentage-change">
+    <p>Percent Change:</p>
+    <p>1hr {percent_change_1h}</p>
+    <p>24hr {percent_change_24h}</p>
+    <p>7d {percent_change_7d}</p>
+  </div>
+</section>
+
+<style>
+  .card-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  }
+  .title {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  }
+  .price {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  }
+  .percentage-change {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
-</p>
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch('https://api.coinlore.com/api/tickers/')
+    const { data } = await res.json()
+    return {
+      props: {
+        currencies: data,
+      },
+    }
+  }
+</script>
+
+<script>
+  import CryptoCard from '$lib/components/crypto-card.svelte'
+
+  export let currencies
+</script>
+
+<main class="main-wrapper">
+  {#each currencies as currency}
+    <CryptoCard {currency} />
+  {/each}
+</main>
+
+<style>
+  .main-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+</style>
